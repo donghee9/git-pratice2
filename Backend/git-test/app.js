7e0f8f31bcbb003a16e5f104aa299f1cd3efd2aa
mac <mac@macui-MacBookAir.local>
2023-06-22 17:55:48 +0900

    ADD:signup 기능 구현

--- Backend/git-test/app.js
+++ Backend/git-test/app.js
@@ -1,29 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const { DataSource } = require('typeorm')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { DataSource } = require("typeorm");
 
 const app = express();
 const myDataSource = new DataSource({
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-})
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/ping', (req, res) => {
-  res.json({ messgae: 'pong' });
-})
+app.get("/ping", (req, res) => {
+    res.json({ messgae: "pong" });
+});
 
 /*
 [TEST]
@@ -31,16 +31,33 @@ app.get('/ping', (req, res) => {
 feature/signin 브랜치의 경우 app.post('/users/signin', ...)
 feature/signup 브랜치의 경우 app.post('/users/signup', ...)
 */
+app.post("/user/signup", async function (req, res) {
+    const { email, password } = req.body;
 
+    await appDataSource.query(
+        `
+     INSERT INTO users (
+       email,
+       password
+     ) VALUES (
+       ?,?
+     )
+   `,
+        [email, password]
+    );
+
+    res.json({ message: "sign-in welcome!" });
+});
 
 app.listen(PORT, () => {
-  myDataSource.initialize()
-    .then(() => {
-      console.log("DB Connection has been initialized")
-    })
-    .catch(() => {
-      console.log("DB Connection has been failed")
-    })
+    myDataSource
+        .initialize()
+        .then(() => {
+            console.log("DB Connection has been initialized");
+        })
+        .catch(() => {
+            console.log("DB Connection has been failed");
+        });
 
-  console.log(`Listening to request on localhost:${PORT}`);
-})
+    console.log(`Listening to request on localhost:${PORT}`);
+});
